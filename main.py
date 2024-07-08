aea2adf46476d78582e51a9984fa563c8798bc8b
ramses2099 <ramses2099@gmail.com>
2024-07-07 23:28:34 -0400

    last commit

--- main.py
+++ main.py
@@ -24,21 +24,40 @@ class Game:
     
     self.player_group = pygame.sprite.Group()
     self.npc_group = pygame.sprite.Group()
-    self.player_group.add(self.player)
     
     # Initialize the first level
-    self.current_level_number = 1
-    self.current_level = Level(self.current_level_number, self.player, self.npc_group)
+    self.player, self.current_level_number, self.current_level = self.reset_game(self.player_group, self.npc_group)
+    
+  def reset_game(self, player_group, npc_group):
+    # Reset player state
+    player = Player(WIDTH/2, 200)
+    player_group.empty()
+    player_group.add(player)
+
+    # Clear NPCs
+    npc_group.empty()
+
+    # Initialize the first level
+    current_level_number = 1
+    current_level = Level(current_level_number, player, npc_group)
+
+    return player, current_level_number, current_level
   
   def showPlayerLives(self):    
     lives = self.player.lives    
     lives_text = self.font.render(f"LIVES: {lives}", True, WHITE)   
     self.screen.blit(lives_text, ((WIDTH-110), 5))
+  
+  def showGameOver(self):       
+    gover_text = self.font.render("- Game Over -", True, WHITE)
+    pagain_text = self.font.render("- Press Enter to play Again -", True, WHITE)   
+    self.screen.blit(gover_text, ((WIDTH //2) - 50, (HEIGHT // 2)- 20))
+    self.screen.blit(pagain_text, ((WIDTH //2) - 150, (HEIGHT // 2)+ 20))
    
   def showPlayerPoints(self):    
-    points = self.player.lives    
-    points_text = self.font.render(f"LIVES: {points}", True, WHITE)   
-    self.screen.blit(points_text, ((WIDTH-110), 5))
+    points = self.player.points    
+    points_text = self.font.render(f"POINTS: {points}", True, WHITE)   
+    self.screen.blit(points_text, ((WIDTH-120), 25))
       
   def showLevel(self):    
     lvl = self.current_level_number    
@@ -56,6 +75,8 @@ class Game:
 
   def run(self):
     running = True
+    game_over = False
+    
     while running:
         
         # Get the delta time (time passed since last frame) in seconds
@@ -70,44 +91,54 @@ class Game:
                 if event.key == pygame.K_SPACE:
                     # Player shoot
                     self.player.shoot()
-                     
-                      
+                elif event.key == pygame.K_RETURN and game_over:
+                    # Initialize the first level
+                    self.player, self.current_level_number, current_level = self.reset_game(self.player_group, self.npc_group)
+                    game_over = False
+        
         # Fill the screen with black color
         self.screen.fill(BLACK)
-        
+            
         # Show LEVEL
         self.showLevel()
         self.showPlayerLives()
+        self.showPlayerPoints()
         
         # DEBUG SHOW FPS
         if (DEBUG):
             self.showFPS()
+         
+        # Game over          
+        if not game_over:
+            # Update game objects (call update methods)
+            # Update player and npc with delta time
+            self.player_group.update(dt)
+            for npc in self.npc_group:
+                npc.update(self.player, dt)
+                    
+            # Update spawer
+            # Update current level
+            if self.current_level.update(dt):
+                # Move to the next level
+                self.current_level_number += 1
+                self.current_level = Level(self.current_level_number, self.player, self.npc_group)
+                self.npc_group.empty()  # Clear NPCs from the previous level
 
-        # Update game objects (call update methods)
-        # Update player and npc with delta time
-        self.player_group.update(dt)
-        for npc in self.npc_group:
-            npc.update(self.player, dt)
-                   
-        # Update spawer
-        # Update current level
-        if self.current_level.update(dt):
-            # Move to the next level
-            self.current_level_number += 1
-            self.current_level = Level(self.current_level_number, self.player, self.npc_group)
-            self.npc_group.empty()  # Clear NPCs from the previous level
-
-        # Update bullet collisions
-        for bullet in self.player.bullets:
-            if pygame.sprite.spritecollide(bullet, self.npc_group, True):
-                bullet.kill()
-                self.player.add_points(10)  # Add points for killing an NP
-
-        for npc in self.npc_group:
-            for bullet in npc.bullets:
-                if pygame.sprite.spritecollide(bullet, self.player_group, False):
+            # Update bullet collisions
+            for bullet in self.player.bullets:
+                if pygame.sprite.spritecollide(bullet, self.npc_group, True):
                     bullet.kill()
-                    self.player.lose_life()
+                    self.player.add_points(10)  # Add points for killing an NP
+
+            for npc in self.npc_group:
+                for bullet in npc.bullets:
+                    if pygame.sprite.spritecollide(bullet, self.player_group, False):
+                        bullet.kill()
+                        self.player.lose_life()
+                        
+            # End game if player is out of lives
+            if not self.player.alive():
+                game_over = True
 
 
         # Draw game objects (call draw methods)
@@ -116,8 +147,10 @@ class Game:
         self.player.bullets.draw(self.screen)
         for npc in self.npc_group:
             npc.bullets.draw(self.screen)
-        
-
+            
+        if game_over:
+            self.showGameOver()
+            
         # Update the display
         pygame.display.flip()
 
